a5ee9f2600da073d243a131e9888d952bccc23f9
Age Manning <Age@AgeManning.com>
2021-06-15 14:40:43 +1000

    Discovery patch (#2382)
    * Upgrade libp2p and unstable gossip
    
    * Network protocol upgrades
    
    * Correct dependencies, reduce incoming bucket limit
    
    * Clean up dirty DHT entries before repopulating
    
    * Update cargo lock
    
    * Update lockfile
    
    * Update ENR dep
    
    * Update deps to specific versions
    
    * Update test dependencies
    
    * Update docker rust, and remote signer tests
    
    * More remote signer test fixes
    
    * Temp commit
    
    * Update discovery
    
    * Remove cached enrs after dialing
    
    * Increase the session capacity, for improved efficiency

--- beacon_node/eth2_libp2p/src/peer_manager/mod.rs
+++ beacon_node/eth2_libp2p/src/peer_manager/mod.rs
@@ -582,14 +582,7 @@ impl<TSpec: EthSpec> PeerManager<TSpec> {
             // ENR's may have multiple Multiaddrs. The multi-addr associated with the UDP
             // port is removed, which is assumed to be associated with the discv5 protocol (and
             // therefore irrelevant for other libp2p components).
-            let mut out_list = enr.multiaddr();
-            out_list.retain(|addr| {
-                addr.iter()
-                    .find(|v| matches!(v, MProtocol::Udp(_)))
-                    .is_none()
-            });
-
-            out_list
+            enr.multiaddr_tcp()
         } else {
             // PeerId is not known
             Vec::new()
@@ -680,6 +673,8 @@ impl<TSpec: EthSpec> PeerManager<TSpec> {
             .collect();
         for peer_id in &peers_to_dial {
             debug!(self.log, "Dialing cached ENR peer"; "peer_id" => %peer_id);
+            // Remove the ENR from the cache to prevent continual re-dialing on disconnects
+            self.discovery.remove_cached_enr(&peer_id);
             self.dial_peer(peer_id);
         }
     }
